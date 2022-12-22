9f40ae09c5c2425af43b97f6f6ac601a2e5be887
Group <groupzakub@gmail.com>
2022-12-22 19:02:37 +0700

    first commit

---
+++ components/SpeechButton.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+export const SpeechButton: React.FC<{ label: string; filename: string }> = ({
+  label,
+  filename,
+}) => {
+  const play = () => {
+    let sound = document.getElementById(filename) as HTMLAudioElement;
+
+    sound.pause();
+    sound.currentTime = 0;
+    sound.play();
+  };
+
+  return (
+    <>
+      <button
+        className="text-xl text-center px-4 py-2 rounded bg-red-500 m-4"
+        onClick={play}
+      >
+        {label}
+      </button>
+      <audio id={filename} preload="metadata">
+        <source src={`/sounds/${filename}.mp3`} type="audio/mpeg" />
+      </audio>
+    </>
+  );
+};
